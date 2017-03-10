e18e731ad81a11d5cc16c8952b4720a4ff9206dd
Omar Wael <omar_bazaraa@hotmail.com>
2017-03-10 22:45:40 +0200

    Initial design for the home page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="text-center">
+        <a href="#header" title="Go to the top of the page">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a>
+        <h4>{{ config('app.name') }} &copy; Copyright 2017</h4>
+    </div>
+</footer>
