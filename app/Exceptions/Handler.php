855239295b23b43f95a2950823c881640cb4e282
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2017-03-15 18:06:52 +0200

    Handle notfoundexception in routing to redirect to home page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException)
+        {
+            return redirect('/');
+        }
         return parent::render($request, $exception);
     }
 
