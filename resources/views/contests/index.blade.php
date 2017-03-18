17bd3764b0b433164367abb521cf2493d6faf55c
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2017-03-18 23:02:10 +0200

    Add new button to contests page

--- resources/views/contests/index.blade.php
+++ resources/views/contests/index.blade.php
@@ -5,7 +5,10 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Contests</div>
+                    <a href="{{url('/contest/add')}}"><span class="pull-right text-dark btn btn-link margin-5px">New</span></a>
+                    <div class="panel-heading">
+                        Contests
+                    </div>
 
                     <div class="panel-body">
                         List of all contests
