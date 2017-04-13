94e667c18315352efba536ef58485bc5cc4b15d3
Omar Wael <omar_bazaraa@hotmail.com>
2017-04-13 20:46:12 +0200

    Make problems table of full width but left it commented

--- resources/views/problems/filters.blade.php
+++ resources/views/problems/filters.blade.php
@@ -1,62 +1,78 @@
-<div class="container problems-filters-container">
-    <div class="row">
-        <div class="col-md-12">
-            <form action="{{ Request::url() }}" method="get" role="form">
+<div class="panel panel-default problems-filters-panel">
+    <div class="panel-heading">Filters</div>
+    <div class="panel-body">
+        <form action="{{ Request::url() }}" method="get" role="form">
 
-                {{--Search Bar--}}
-                <div id="custom-search-input">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Problem Name"
-                               name="{{ Constants::URL_QUERY_SEARCH_KEY }}"
-                               value="{{ Request::get(Constants::URL_QUERY_SEARCH_KEY) }}">
+            {{--Search Bar--}}
+            <div id="custom-search-input">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Problem Name"
+                           name="{{ Constants::URL_QUERY_SEARCH_KEY }}"
+                           value="{{ Request::get(Constants::URL_QUERY_SEARCH_KEY) }}">
 
-                        <span class="input-group-btn">
-                            <button class="btn btn-default btn-lg" type="submit">
-                                <i class="glyphicon glyphicon-search"></i>
-                            </button>
-                        </span>
+                    <span class="input-group-btn">
+                        <button class="btn btn-default btn-lg" type="submit">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
+
+            {{--Judges checkboxes--}}
+            <div>
+                <h4>Online Judges:</h4>
+                @foreach ($judges as $judge)
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" {{ in_array($judge->id, Request::get(Constants::URL_QUERY_JUDGES_KEY, [])) ? 'checked' : '' }}
+                                   name="{{ Constants::URL_QUERY_JUDGES_KEY }}[]"
+                                   value="{{ $judge->id }}">
+                            {{ $judge->name }}
+                        </label>
                     </div>
-                </div>
+                @endforeach
+            </div>
 
-                {{--Judges checkboxes--}}
-                <div>
-                    <h4>Online Judges:</h4>
-                    @foreach ($judges as $judge)
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" {{ in_array($judge->id, Request::get(Constants::URL_QUERY_JUDGES_KEY, [])) ? 'checked' : '' }}
-                                       name="{{ Constants::URL_QUERY_JUDGES_KEY }}[]"
-                                       value="{{ $judge->id }}">
-                                {{ $judge->name }}
-                            </label>
-                        </div>
-                    @endforeach
-                </div>
+            {{--Tags checkboxes--}}
+            {{--<div class="container" style="padding: 0">--}}
+                {{--<h4>Tags:</h4>--}}
+                {{--<div class="row">--}}
+                    {{--@foreach ($tags as $tag)--}}
+                        {{--<div class="col-sm-3 checkbox" style="margin-top: 0">--}}
+                            {{--<label>--}}
+                                {{--<input type="checkbox" {{ in_array($tag->id, Request::get(Constants::URL_QUERY_TAGS_KEY, [])) ? 'checked' : '' }}--}}
+                                       {{--name="{{ Constants::URL_QUERY_TAGS_KEY }}[]"--}}
+                                       {{--value="{{ $tag->id }}">--}}
+                                {{--{{ $tag->name }}--}}
+                            {{--</label>--}}
+                        {{--</div>--}}
+                    {{--@endforeach--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
-                {{--ToDo: Samir Tags Checkboxes but I will change this later isA to autocomplete--}}
-                {{--Tags checkboxes--}}
-                <div>
-                    <h4>Tags:</h4>
-                    @foreach ($tags as $tag)
-                        <div class="checkbox problems-filters-tags-checkboxes">
-                            <label>
-                                <input type="checkbox" {{ in_array($tag->id, Request::get(Constants::URL_QUERY_TAGS_KEY, [])) ? 'checked' : '' }}
-                                       name="{{ Constants::URL_QUERY_TAGS_KEY }}[]"
-                                       value="{{ $tag->id }}">
-                                {{ $tag->name }}
-                            </label>
-                        </div>
-                    @endforeach
-                </div>
+            {{--ToDo: Samir tags checkboxes but I will change this later isA to autocomplete--}}
+            {{--Tags checkboxes--}}
+            <div>
+                <h4>Tags:</h4>
+                @foreach ($tags as $tag)
+                    <div class="checkbox problems-filters-tags-checkboxes">
+                        <label>
+                            <input type="checkbox" {{ in_array($tag->id, Request::get(Constants::URL_QUERY_TAGS_KEY, [])) ? 'checked' : '' }}
+                                   name="{{ Constants::URL_QUERY_TAGS_KEY }}[]"
+                                   value="{{ $tag->id }}">
+                            {{ $tag->name }}
+                        </label>
+                    </div>
+                @endforeach
+            </div>
 
-                <hr/>
+            <hr/>
 
-                {{--Apply filters & Clear buttons--}}
-                <p>
-                    <input class="btn btn-default" type="submit" value="Apply Filters"/>
-                    <a href="{{ Request::url() }}" class="btn btn-link text-dark pull-right">Clear</a>
-                </p>
-            </form>
-        </div>
+            {{--Apply filters & Clear buttons--}}
+            <p>
+                <input class="btn btn-default pull-right" type="submit" value="Apply Filters"/>
+                <a href="{{ Request::url() }}" class="btn btn-link text-dark">Clear</a>
+            </p>
+        </form>
     </div>
 </div>
--- resources/views/problems/table.blade.php
+++ resources/views/problems/table.blade.php
@@ -1,65 +1,59 @@
-<div class="container problems-table-container">
-    <table class="table table-bordered">
-        {{--Headings--}}
-        <thead>
-            <tr>
-                @include('problems.sortable_heading', ['title' => 'ID', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_ID_KEY])
-                @include('problems.sortable_heading', ['title' => 'Name', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_NAME_KEY])
-                @include('problems.sortable_heading', ['title' => '#Acc.', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_ACCEPTED_COUNT_KEY])
-                @include('problems.sortable_heading', ['title' => 'Judge', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_JUDGE_KEY])
+<table class="table table-bordered">
+    {{--Headings--}}
+    <thead>
+        <tr>
+            @include('problems.sortable_heading', ['title' => 'ID', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_ID_KEY])
+            @include('problems.sortable_heading', ['title' => 'Name', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_NAME_KEY])
+            @include('problems.sortable_heading', ['title' => '#Acc.', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_ACCEPTED_COUNT_KEY])
+            @include('problems.sortable_heading', ['title' => 'Judge', 'sortParam' => Constants::URL_QUERY_SORT_PARAM_JUDGE_KEY])
+
+            {{--Tags--}}
+            <th class="problems-table-head problems-table-head-tags">Tags</th>
+        </tr>
+    </thead>
+
+    @php($user = Auth::user())
+
+    {{--Problems--}}
+    <tbody>
+        {{--TODO: @Samir add checkboxes When adding new contest view--}}
+        @foreach($problems as $problem)
+            @php($verdict = $problem->simpleVerdict($user))
+
+            <tr class="{{ $verdict == Constants::SIMPLE_VERDICT_ACCEPTED ? 'success' : ($verdict == Constants::SIMPLE_VERDICT_WRONG_SUBMISSION ? 'danger' : '') }}">
+                {{--ID--}}
+                <td>{{ Utilities::generateProblemNumber($problem) }}</td>
+
+                {{--Name--}}
+                <td>
+                    <a href="{{ Utilities::generateProblemLink($problem) }}" target="_blank">
+                        {{ $problem->name }}
+                    </a>
+                </td>
+
+                {{--# Accepted--}}
+                <td>{{ $problem->solved_count }}</td>
+
+                {{--Judge--}}
+                <td>
+                    <a href="{{ Constants::JUDGES[$problem->judge_id][Constants::JUDGE_LINK_KEY] }}" target="_blank">
+                        {{ Constants::JUDGES[$problem->judge_id][Constants::JUDGE_NAME_KEY] }}
+                    </a>
+                </td>
 
                 {{--Tags--}}
-                <th class="problems-table-head problems-table-head-tags">Tags</th>
+                <td>
+                    @foreach($problem->tags()->get() as $tag)
+                        <a class="problems-table-tag-link"
+                           href="{{ Request::url() . '?' . http_build_query([Constants::URL_QUERY_TAG_KEY => $tag->id]) }}">
+                            {{ $tag->name }}
+                        </a>
+                    @endforeach
+                </td>
             </tr>
-        </thead>
+        @endforeach
+    </tbody>
+</table>
 
-        @php
-            $user = Auth::user();
-        @endphp
-
-        {{--Problems--}}
-        <tbody>
-            {{--TODO (Samir) Adding CheckBoxes When Adding New Contest View--}}
-            @foreach($problems as $problem)
-                @php
-                    $verdict = $problem->simpleVerdict($user);
-                @endphp
-
-                <tr class="{{ $verdict == Constants::SIMPLE_VERDICT_ACCEPTED ? 'success' : ($verdict == Constants::SIMPLE_VERDICT_WRONG_SUBMISSION ? 'danger' : '') }}">
-                    {{--ID--}}
-                    <td>{{ Utilities::generateProblemNumber($problem) }}</td>
-
-                    {{--Name--}}
-                    <td>
-                        <a href="{{ Utilities::generateProblemLink($problem) }}" target="_blank">
-                            {{ $problem->name }}
-                        </a>
-                    </td>
-
-                    {{--# Accepted--}}
-                    <td>{{ $problem->solved_count }}</td>
-
-                    {{--Judge--}}
-                    <td>
-                        <a href="{{ Constants::JUDGES[$problem->judge_id][Constants::JUDGE_LINK_KEY] }}" target="_blank">
-                            {{ Constants::JUDGES[$problem->judge_id][Constants::JUDGE_NAME_KEY] }}
-                        </a>
-                    </td>
-
-                    {{--Tags--}}
-                    <td>
-                        @foreach($problem->tags()->get() as $tag)
-                            <a class="problems-table-tag-link"
-                               href="{{ Request::url() . '?' . http_build_query([Constants::URL_QUERY_TAG_KEY => $tag->id]) }}">
-                                {{ $tag->name }}
-                            </a>
-                        @endforeach
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-    {{--Pagination--}}
-    {{ $problems->appends(Request::all())->render() }}
-</div>
+{{--Pagination--}}
+{{ $problems->appends(Request::all())->render() }}
