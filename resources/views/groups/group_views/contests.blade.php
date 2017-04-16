12545c92945e2704c6944e0af584dca2651c42eb
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2017-04-16 15:32:03 +0200

    Fix group contests pagiantion

--- resources/views/groups/group_views/contests.blade.php
+++ resources/views/groups/group_views/contests.blade.php
@@ -1,27 +1,29 @@
 {{--Display single group contests info--}}
-<table class="table table-bordered text-center" id="contests_table">
-    <thead>
-    <tr>
-        <th class="text-center">ID</th>
-        <th class="text-center contest-table-name-th">Name</th>
-        <th class="text-center">Time</th>
-        <th class="text-center">Duration</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($data[Constants::CONTESTS_CONTESTS_KEY] as $contest)
+<div class="text-center">
+    <table class="table table-bordered" id="contests_table">
+        <thead>
         <tr>
-            <td>{{ $contest->id }}</td>
-            <td>
-                <a href="{{ url('contest/' . $contest->id) }}">
-                    {{ $contest->name }}
-                </a>
-            </td>
-            <td>{{ date('D M d, H:i', strtotime($contest->time))}}</td>
-            <td>{{ \App\Utilities\Utilities::convertMinsToHoursMins($contest->duration) }} hrs</td>
+            <th class="text-center">ID</th>
+            <th class="text-center contest-table-name-th">Name</th>
+            <th class="text-center">Time</th>
+            <th class="text-center">Duration</th>
         </tr>
-    @endforeach
-    </tbody>
-</table>
-{{--Pagination--}}
-{{ $data[Constants::CONTESTS_CONTESTS_KEY]->render() }}
+        </thead>
+        <tbody>
+        @foreach($data[Constants::CONTESTS_CONTESTS_KEY] as $contest)
+            <tr>
+                <td>{{ $contest->id }}</td>
+                <td>
+                    <a href="{{ url('contest/' . $contest->id) }}">
+                        {{ $contest->name }}
+                    </a>
+                </td>
+                <td>{{ date('D M d, H:i', strtotime($contest->time))}}</td>
+                <td>{{ \App\Utilities\Utilities::convertMinsToHoursMins($contest->duration) }} hrs</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{--Pagination--}}
+    {{ $data[Constants::CONTESTS_CONTESTS_KEY]->fragment('contests')->links() }}
+</div>
