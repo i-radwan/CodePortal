ae239629f11b3dfbe559bc7edd35bc6236f3f3a4
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2017-04-18 17:42:55 +0200

    Reposlishing samir code (intial, will be continued today @ 9PM on Google Hangouts after samir fixes some stuff)

--- public/js/script.js
+++ public/js/script.js
@@ -275,3 +275,118 @@ function hideNotificationElement(element) {
 /**************************************/
 /*</editor-fold>*/
 
+/*<editor-fold desc="Add contest">*/
+
+//Tags AutoComplete parameters
+var tagsList = document.getElementById("tagsList");
+$('input.tagsAuto').typeahead(autoComplete($("#tagsAuto").data('tags-path'), tagsList, "tags[]", 0));
+
+function applyFilters(url, token) {
+    var filters = getCurrentFilters();
+    $.ajax({
+        // url: "{{Request::url()}}/TagsJudgesFSync",
+        url: url,
+        type: 'POST',
+        data: {
+            _token: token,
+            cProblemsFilters: filters,
+        },
+        success: function (data) {
+        }
+    });
+//        location.reload();
+    document.getElementById("clearTableLink").click();
+}
+//Wait for deletion key
+$(document).on('mousedown', '.tags-close-icon', function (item) {
+    $(this).parent().remove();
+});
+//get Current Entered Form Data
+//    function getEnteredFormData() {
+//        var conName = document.getElementById("name").value;
+//        var conDate = document.getElementById("time").value;
+//        var conDur = document.getElementById("duration").value;
+//        var conVis = document.getElementById("private").value;
+//        return {
+//            name : conName,
+//            date : conDate,
+//            duration: conDur,
+//            visibility : conVis
+//        };
+//    }
+
+//Tags AutoComplete parameters
+function autoComplete(path, list, arrName, type) {
+    return ({
+        source: function (query, process) {
+            return $.get(path, {query: query}, function (data) {
+                return process(data);
+            })
+        },
+        updater: function (item) {
+            //Get the current values of list items in the unordered list 'tagsList'
+            var currentItems = list.getElementsByTagName('li');
+            var itemName = (item.name) ? item.name : item;
+            //check if it's already included
+            var notFound = true;
+            console.log(currentItems, itemName);
+            for (var i = 0; i < currentItems.length; i++) {
+                if (currentItems[i].textContent.trim() == itemName.trim()) {
+                    notFound = false;
+                }
+            }
+            if (notFound) {
+                //Create a new list item li
+                var entry = document.createElement('li');
+                entry.setAttribute("name", arrName);
+                entry.setAttribute("value", itemName);
+                //Add the item name and the delete button
+                if (type == 1)
+                    var text = '<button class="organiser-close-icon "></button>';
+                else
+                    var text = '<button class="tags-close-icon "></button>';
+                entry.innerHTML = text + itemName;
+                list.appendChild(entry);
+                if (type == 1) {
+                    applyOrganisers();
+                }
+
+            }
+            //Don't return the item name back in order not to keep it in the text box field
+            return;
+        }
+    });
+}
+function getListInfo(list) {
+    //Reading list elements
+    var currentItems = list.getElementsByTagName('li');
+    var elements = [];
+    for (var i = 0; i < currentItems.length; i++) {
+        elements[i] = currentItems[i].textContent;
+    }
+    return elements;
+}
+function getCurrentFilters() {
+    //Reading Tags
+    var tags = getListInfo(tagsList);
+    //Reading Judges info
+    var judges = [];
+    var j = 0;
+    var checkboxes = document.getElementsByClassName('judgeState');
+    for (var i = 0; checkboxes[i]; ++i) {
+        if (checkboxes[i].checked) {
+            judges[j] = checkboxes[i].value;
+            j = j + 1;
+        }
+    }
+    //Then you have now judges and tags
+    return ({'cTags': tags, 'cJudges': judges});
+}
+
+// ToDo re-polishing needed
+$('.pagination > li').click(function () {
+    // ToDo save form
+    sessionStorage.setItem("name", $("#name").val());
+});
+/**************************************/
+/*</editor-fold>*/
