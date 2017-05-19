6f70eede20a41830b6db538ff530aaeac17070ba
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2017-05-19 20:59:50 +0200

    Add sync handles service file

---
+++ app/Console/Commands/SyncNewHandles.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\CodeforcesSyncService;
+use App\Services\LiveArchiveSyncService;
+use App\Services\UVaSyncService;
+use App\Utilities\Constants;
+use Illuminate\Console\Command;
+
+class SyncNewHandles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:handles-submissions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch new handles from the db queue to sync their submissions!';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Get user handles to sync their submissions from database table
+        $handles = \DB::table(Constants::TBL_HANDLES_SYNC_QUEUE)
+            ->select()
+            ->limit(5)
+            ->get();
+
+        // iterate over the queue first 5 handles to sync their submissions
+        $handles->each(function ($handle) {
+            $userID = $handle[Constants::FLD_HANDLES_SYNC_QUEUE_USER_ID];
+            $judgeID = $handle[Constants::FLD_HANDLES_SYNC_QUEUE_JUDGE_ID];
+
+            switch ($judgeID) {
+                case Constants::JUDGE_CODEFORCES_ID:
+                    $syncService = new CodeforcesSyncService();
+                    break;
+                case Constants::JUDGE_UVA_ID:
+                    $syncService = new UVaSyncService();
+                    break;
+                case Constants::JUDGE_LIVE_ARCHIVE_ID:
+                    $syncService = new LiveArchiveSyncService();
+                    break;
+            }
+            $syncStatus = $syncService->syncSubmissions(User::find($userID));
+
+            // Remove the handle from the table if sync succeeded
+            if ($syncStatus) {
+                \DB::table(Constants::TBL_HANDLES_SYNC_QUEUE)
+                    ->where(Constants::FLD_HANDLES_SYNC_QUEUE_USER_ID, $userID)
+                    ->where(Constants::FLD_HANDLES_SYNC_QUEUE_JUDGE_ID, $judgeID)
+                    ->delete();
+            }
+        });
+    }
+}
